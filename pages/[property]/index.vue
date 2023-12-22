06dcc966109ca02ccf4dc0893003d6adeb548783
irfanfitriandi <irfanfitriandi@gmail.com>
2023-12-22 17:01:44 +0700

    feat: assets img

--- pages/[property]/index.vue
+++ pages/[property]/index.vue
@@ -1,5 +1,68 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const DB = [
+  {
+    name: 'madani',
+    desc: '',
+    address: '',
+    img: {
+      cover: [],
+      spek: [],
+      site: [],
+      drone: [],
+      detail: [],
+    },
+  },
+  {
+    name: 'permata',
+    desc: '',
+    address: '',
+    img: {
+      cover: [],
+      spek: [],
+      site: [],
+      drone: [],
+      detail: [],
+    },
+  },
+  {
+    name: 'zhafira',
+    desc: '',
+    address: '',
+    img: {
+      cover: [],
+      spek: [],
+      site: [],
+      drone: [],
+      detail: [],
+    },
+  },
+  {
+    name: 'damaido',
+    desc: '',
+    address: '',
+    img: {
+      cover: [],
+      spek: [],
+      site: [],
+      drone: [],
+      detail: [],
+    },
+  },
+  {
+    name: 'green-res',
+    desc: '',
+    address: '',
+    img: {
+      cover: [],
+      spek: [],
+      site: [],
+      drone: [],
+      detail: [],
+    },
+  },
+]
+</script>
 
 <template>
-  <div>--PAGE UNDER CONSTRUCTION--</div>
+  <div class="text-center">--PAGE UNDER CONSTRUCTION--</div>
 </template>
